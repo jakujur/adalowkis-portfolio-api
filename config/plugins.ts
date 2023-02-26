3d9fa64cd68e75abbc2585abc787a851375201c1
jakubjurczak <jakub.jurczak@wayfdigital.com>
2023-02-26 14:37:21 +0100

    feat: add aws services

---
+++ config/plugins.ts
@@ -0,0 +1,15 @@
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_ACCESS_SECRET,
+        region: process.env.AWS_REGION,
+        params: {
+          Bucket: process.env.AWS_BUCKET_NAME,
+        },
+      },
+    },
+  },
+});
